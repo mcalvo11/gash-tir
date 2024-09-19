3f880fbb7b9a589581e2a537934d0e21e2935a74
Mariela <mcalvo@pernix-solutions.com>
2024-09-19 10:20:03 -0600

    Configure aws

---
+++ src/aws-exports.js
@@ -0,0 +1,12 @@
+const awsconfig = {
+  aws_project_region: 'us-east-2',
+  region: 'us-east-2',
+  aws_cognito_identity_pool_id: 'us-east-2:530cc26e-5e91-44ff-b97c-e038156e5945',
+  aws_cognito_region: 'us-east-2',
+  aws_s3_bucket: 'examplebucket2s',
+  aws_s3_region: 'us-east-2',
+  aws_user_files_s3_bucket_region: 'us-east-2',
+  aws_rekognition_region: 'us-east-2'
+};
+
+export default awsconfig;

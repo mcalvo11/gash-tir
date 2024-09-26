464c751a71831dcbd9910cb41f5a0c7127db0c37
Stacy30 <stacy@pernixlabs.com>
2024-09-26 13:15:36 -0600

    Add functionality to take photos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 import { Amplify, Storage } from 'aws-amplify';
 import { useDropzone } from 'react-dropzone';
 import awsconfig from './aws-exports';
@@ -6,10 +6,11 @@ import './App.css';
 import ContainerInfoTable from './ContainerInfoTable';
 import DataInfoConfidence from './DataInfoConfidence';
 import detectText from "./rekognition";
+import Webcam from 'react-webcam';
+
 Amplify.configure(awsconfig);
 
 function App() {
-  const [fileUrl, setFileUrl] = useState('');
   const [previewUrl, setPreviewUrl] = useState('');
   const [detectedTexts, setDetectedTexts] = useState([]);
   const [containerData, setContainerData] = useState({
@@ -19,20 +20,23 @@ function App() {
     verifier: '',
     isoType: '',
   });
+  const webcamRef = useRef(null);
+  const [isWebcamOpen, setIsWebcamOpen] = useState(false);
+
   const onDrop = useCallback(async (acceptedFiles) => {
     const file = acceptedFiles[0];
-    
+
     if (file) {
       const fileName = file.name;
-      setPreviewUrl(URL.createObjectURL(file)); 
-  
+      setPreviewUrl(URL.createObjectURL(file));
+
       try {
         const result = await Storage.put(fileName, file, {
           contentType: file.type,
           level: 'public',
         });
         const detectedTextResults = await detectText(fileName);
-        setDetectedTexts(detectedTextResults); 
+        setDetectedTexts(detectedTextResults);
         if (detectedTextResults.length >= 4) {
           const ownerAndType = detectedTextResults[0].text;
           const newContainerData = {
@@ -45,19 +49,77 @@ function App() {
           setContainerData(newContainerData);
         }
 
-        const url = await Storage.get(result.key);
-        setFileUrl(url);
+        await Storage.get(result.key);
       } catch (err) {
         console.error('Error al subir el archivo:', err);
       }
     }
   }, []);
 
+  const openWebcamModal = () => {
+    setIsWebcamOpen(true);
+  };
+
+  const closeWebcamModal = () => {
+    setIsWebcamOpen(false);
+    if (webcamRef.current && webcamRef.current.video && webcamRef.current.video.srcObject) {
+      const stream = webcamRef.current.video.srcObject;
+      const tracks = stream.getTracks();
+      tracks.forEach(track => track.stop());
+    }
+  };
+
+  const capturePhoto = async () => {
+    if (webcamRef.current) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setPreviewUrl(imageSrc);
+      const fileName = `photo-${Date.now()}.jpg`;
+
+      try {
+        const response = await fetch(imageSrc);
+        const blob = await response.blob();
+
+        const result = await Storage.put(fileName, blob, {
+          contentType: 'image/jpeg',
+          level: 'public',
+        });
+
+        console.log('Foto subida con éxito:', result);
+
+        const detectedTextResults = await detectText(fileName);
+        setDetectedTexts(detectedTextResults);
+
+        if (detectedTextResults.length >= 4) {
+          const ownerAndType = detectedTextResults[0].text;
+          const newContainerData = {
+            owner: ownerAndType.slice(0, 3),
+            type: ownerAndType.slice(3, 4),
+            id: detectedTextResults[1].text,
+            verifier: detectedTextResults[2].text,
+            isoType: detectedTextResults[3].text,
+          };
+          setContainerData(newContainerData);
+        }
+
+        await Storage.get(result.key);
+
+      } catch (err) {
+        console.error('Error al subir la foto:', err);
+      }
+
+      closeWebcamModal();
+    }
+  };
+
+  const videoConstraints = {
+    facingMode: 'environment',
+  };
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     accept: 'image/*',
   });
+
   return (
     <div className="App">
       <div className="upload-container" style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -78,16 +140,32 @@ function App() {
                 {isDragActive ? (
                   <p>Arrastre la imagen aquí ...</p>
                 ) : (
-                  <button className="choose-file-btn">Seleccione una imagen</button>
+                    <button className="choose-file-btn">Seleccione una imagen</button>
                 )}
               </div>
             </div>
+            <button className="open-webcam-btn" onClick={openWebcamModal}>Abrir Cámara</button>
           </div>
         </div>
         <div className="table-section">
           <h1>Información del Contenedor</h1>
           <ContainerInfoTable containerData={containerData} />
           <DataInfoConfidence containerData={detectedTexts} />
+        </div>
+      </div>
+      <div className={`modal ${isWebcamOpen ? 'show' : ''}`}>
+        <div className="modal-content">
+          {isWebcamOpen && (
+            <Webcam
+              className="modal-content-inside"
+              ref={webcamRef}
+              audio={false}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+          )}
+          <button className="open-webcam-btn" onClick={capturePhoto}>Tomar Foto</button>
+          <button className="close-modal-btn" onClick={closeWebcamModal}>Cerrar</button>
         </div>
       </div>
     </div>
